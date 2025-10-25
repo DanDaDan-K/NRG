10fc27bc0380fa07f36700f1fad3991346b6e03f
Guest User <ss@Stevens-MacBook-Pro.local>
2025-10-25 17:56:44 +0400

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production'
+const repo = 'plugo'
 
-export default nextConfig;
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : undefined,
+}
+
+export default nextConfig
